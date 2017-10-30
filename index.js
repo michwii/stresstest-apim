d37b30a3f00d61c37fbdfc45666ad82e4f976830
Michwii <freezerhm@yahoo.fr>
2017-10-30 15:19:58 +0100

    adding xml

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var app = express()
+var fs = require('fs');
 
 var objectJSON = {
   status: true,
@@ -21,8 +22,19 @@ app.get('/backend_simple_rest', function (req, res) {
 
 app.get('/backend_simple_soap', function (req, res) {
   objectJSON.id = Math.floor((Math.random() * 10) + 1);
-  setTimeout(sendDelayedResponse.bind(null, res, objectJSON), 100);
+
+  fs.readFile('xml_sample.xml', 'utf8', function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+    setTimeout(sendDelayedResponseXML.bind(null, res, data), 100);
+  });
 });
+
+function sendDelayedResponseXML(res, xml){
+  res.status(200) ;
+  res.send(xml);
+}
 
 function sendDelayedResponse(res, json){
   res.status(200) ;
