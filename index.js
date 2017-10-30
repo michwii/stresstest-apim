0e0545672d10cbe5c30a04628678902a0eb4ff1c
Michwii <freezerhm@yahoo.fr>
2017-10-30 14:27:54 +0100

    avec express

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 var http = require('http');
 
 var server = http.createServer(function(request, response) {
@@ -11,3 +12,33 @@ var port = process.env.PORT || 1337;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
+*/
+
+var express = require('express')
+var app = express()
+
+var objectJSON = {
+  status: true,
+  id : 5,
+  name: "Matthias",
+  surname : "Blanquer",
+  email : "matthias.blanquer@axa.fr"
+}
+
+app.get('/', function (req, res){
+  res.json("Je suis une page index")
+});
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/backend_simple_rest', function (req, res) {
+  objectJSON.id = Math.floor((Math.random() * 10) + 1);
+  setTimeout(sendDelayedResponse.bind(null, res, objectJSON), 100);
+});
+
+function sendDelayedResponse(res, json){
+  res.json(json);
+}
+
+app.listen(process.env.PORT || 1337, function () {
+  console.log('Example app listening on port 80!');
+})
